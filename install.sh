f6a902ffaca95efeff6a994d4f3d268ef7b0721f
iiPython <ben@iipython.cf>
2022-12-28 00:25:21 -0600

    Include fonts and cursors

--- install.sh
+++ install.sh
@@ -39,18 +39,21 @@ function begin_install() {
     status "Installing Chicago95"
     sudo cp -r Chicago95/Theme/Chicago95 /usr/share/themes/
     sudo cp -r Chicago95/Icons/* /usr/share/icons/
+    sudo cp -r Chicago95/Cursors/* /usr/share/icons/
+    sudo cp -r Chicago95/Fonts/bitmap/cronyx-crillic /usr/share/fonts/
 
     status "Installing qt5-qtstyleplugins for qt5 theme support"
     sudo dnf install qt5-qtstyleplugins -y
 
     # Enable Chicago95
     status "Enabling Chicago95"
-    xfconf-query -c xsettings -p /Net/ThemeName -s "Chicago95"          # Theme
-    xfconf-query -c xsettings -p /Net/IconThemeName -s "Chicago95"      # Icon Theme
-    xfconf-query -c xfwm4 -p /general/theme -s "Chicago95"              # WM Theme
-    xfconf-query -c xfwm4 -p /general/title_font -s "Sans Bold 8"       # WM Title Font (Sans Bold 8pt)
-    xfconf-query -c xfce4-notifyd -p /theme -s "Chicago95"              # Notification Theme
-    xfconf-query -c xfce4-notifyd -p /initial-opacity -s "1"            # Notification Opacity (100%)
+    xfconf-query -c xsettings -p /Net/ThemeName -s "Chicago95"                          # Theme
+    xfconf-query -c xsettings -p /Net/IconThemeName -s "Chicago95"                      # Icon Theme
+    xfconf-query -c xsettings -p /Gtk/CursorThemeName -s "Chicago95 Standard Cursors"   # Cursor Theme
+    xfconf-query -c xfwm4 -p /general/theme -s "Chicago95"                              # WM Theme
+    xfconf-query -c xfwm4 -p /general/title_font -s "Sans Bold 8"                       # WM Title Font (Sans Bold 8pt)
+    xfconf-query -c xfce4-notifyd -p /theme -s "Chicago95"                              # Notification Theme
+    xfconf-query -c xfce4-notifyd -p /initial-opacity -s "1"                            # Notification Opacity (100%)
     
     # Import Chicago95 panel layout
     wget https://github.com/iiPythonx/Fedora95/blob/main/fedora95.tar.bz2
